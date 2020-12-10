26bcfd4bb2c4b58e12952c4900eb2da8829d8c62
FRANCESCO <OLIVOTTI>
2020-12-10 15:27:23 +0100

    push 1

--- es6-icons/milestone1/icons.js
+++ es6-icons/milestone1/icons.js
@@ -1,7 +1,39 @@
 $(document).ready(function () {
   // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family 
+class icone {
+
+  constructor( name, prefix, type, family ){
+
+    this.name = name;
+    this.prefix = prefix;
+    this.type = type;
+    this.family = family;
+  }
+}
+
+const Icons = [
+  new icone ("dog", "fas", "fa-dog", "animals"),
+  new icone ("owl", "fas", "fa-owl", "animals"),
+  new icone ("cat", "fas", "fa-cat", "animals"),
+  new icone ("horse", "fas", "fa-horse", "animals"),
+  new icone ("frog", "fas", "fa-frog", "animals"),
+  new icone ("hippo", "fas", "fa-hippo", "animals"),
+  new icone ("rabbit", "fas", "fa-rabbit", "animals"),
+  new icone ("spider", "fas", "fa-spider", "animals"),
+  new icone ("snake", "fas", "fa-snake", "animals"),
+  new icone ("ram", "fas", "fa-ram", "animals"),
+  
+
+];
+
+console.log(Icons)
+
+
+
 
   // Semezioniamo il container icons
+
+const container = document.getElementById('icons')
 
   //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
 
