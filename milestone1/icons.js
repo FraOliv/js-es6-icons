b5e2220ad16b1f0185fe19f09abf6a96426c638b
FRANCESCO <OLIVOTTI>
2020-12-10 18:35:13 +0100

    milestone 1 + milestone 2

--- milestone1/icons.js
+++ milestone1/icons.js
@@ -11,21 +11,26 @@ class icone {
   }
 }
 
-const Icone = [
+const IconsContainer = [
   new icone ("dog", "fas", "fa-dog", "animals"),
   new icone ("dove", "fas", "fa-dove", "animals"),
   new icone ("cat", "fas", "fa-cat", "animals"),
   new icone ("horse", "fas", "fa-horse", "animals"),
-  new icone ("frog", "fas", "fa-frog", "animals"),
   new icone ("hippo", "fas", "fa-hippo", "animals"),
+  new icone("carrot", "fas", "fa-carrot", "vegetables"),
+  new icone("lemon", "fas", "fa-lemon", "vegetables"),
+  new icone("apple", "fas", "fa-apple-alt", "vegetables"),
   new icone ("spider", "fas", "fa-spider", "animals"),
   new icone ("kiwi-bird", "fas", "fa-kiwi-bird", "animals"),
   new icone ("paw", "fas", "fa-paw", "animals"),
   new icone ("otter", "fas", "fa-otter", "animals"),
+  new icone ("compact-disc", "fas", "fa-compact-disc", "computers"),
+  new icone ("database", "fas", "fa-database", "computers"),
+  new icone ("download", "fas", "fa-download", "computers"),
   
 
 ];
-console.log(Icone)
+console.log(IconsContainer)
 
 
 
@@ -37,17 +42,23 @@ console.log(Icone)
 
 const container = $("div.icons");
   //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
-Icone.forEach(element => {
+ function print(array,selector){
 
-  container.append(`
-  <div>
-    <i class = "${element.prefix} ${element.type}"></i>
-  </div>
-  `);
-  
+    array.forEach(element =>{
+
+        selector.append(`
+      <div>
+        <i class="${element.prefix} ${element.type}"></i>
+      </div>`
+      );
+      })
+  }
+
+  const printIcone = print(IconsContainer, container);
+
 });
 
-});
+
 /* ---- FUNCTIONS ----*/
 // Creiamo una funzione per iterare nell'array e che appenda al container le icone.
 // tip: possiamo usare un ciclo foreach qui e destrutturare gli le proprieta degli elementi di un array
--- milestone2/icons.js
+++ milestone2/icons.js
@@ -1,24 +1,59 @@
 $(document).ready(function () {
   // Qui le icone che abbiamo definito nella milestone 1
+  // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family 
+$(function () {
 
-  // definiamo dei colori per le icone (blue, orange, purple)
+  class icone {
+    constructor(name, prefix, type, family) {
+      this.name = name;
+      this.prefix = prefix;
+      this.type = type;
+      this.family = family;
+    }
+  }
 
+  const IconsContainer =   [
+  new icone ("dog", "fas", "fa-dog", "animals"),
+  new icone ("dove", "fas", "fa-dove", "animals"),
+  new icone ("cat", "fas", "fa-cat", "animals"),
+  new icone ("horse", "fas", "fa-horse", "animals"),
+  new icone ("hippo", "fas", "fa-hippo", "animals"),
+  new icone("carrot", "fas", "fa-carrot", "vegetables"),
+  new icone("lemon", "fas", "fa-lemon", "vegetables"),
+  new icone("apple", "fas", "fa-apple-alt", "vegetables"),
+  new icone ("spider", "fas", "fa-spider", "animals"),
+  new icone ("kiwi-bird", "fas", "fa-kiwi-bird", "animals"),
+  new icone ("paw", "fas", "fa-paw", "animals"),
+  new icone ("otter", "fas", "fa-otter", "animals"),
+  new icone ("compact-disc", "fas", "fa-compact-disc", "computers"),
+  new icone ("database", "fas", "fa-database", "computers"),
+  new icone ("download", "fas", "fa-download", "computers"),
+]
 
-  //aggiungiamo dei colori usando una funzione
+  console.log(IconsContainer);
 
+  let container = $(".icons");
+  const blue = "blue";
+  const purple = "purple";
+  const orange = "orange";
 
-  //inseriamo le icone colorate nel container
+  IconsContainer.forEach((icon) => {
+      if (icon.family === "animals") {
+        icon.color = orange;
+      } else if (icon.family === "computers") {
+        icon.color = blue;
+      } else {
+        icon.color = purple;
+      };
+  });
 
-});
+  let print = IconsContainer.forEach((icon) => {
+    container.append(`
+      <div>
+      <i class="${icon.prefix} ${icon.type}" style="color:${icon.color}"></i>
+      <div class="title">${icon.name}</div>
+      </div>
+    `);
+  });
 
-
-/* ---- FUNCTIONS ----*/
-//1. Funzione milestone 1
-
-
-//2. funzione per assegnare un colore ad un icona
-// tip: possiamo invocare qui dentro un'altra funzione (2) oppure usare map 
-
-
-//3. funzione per ottenere da un array
-// tip: possiamo usarla nella funzione precedente oppure usare map senza creare questa funzione.
+})})
